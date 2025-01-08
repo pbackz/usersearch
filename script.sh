5e4a4d7488e2f0789b0b7234cad9a04630818e37
Pierre Baconnier <pierre.baconnier@outlook.fr>
2025-01-08 22:06:56 +0100

    fix: install script

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 npm install
-mkdir gosearch && cp -a go.mod go.sum gosearch.go data.json gosearch/ && go build && PATH=$PATH:$(pwd)
+mkdir gosearch && cp -a go.mod go.sum gosearch.go data.json gosearch/
+go build && PATH=$PATH:$(pwd)
